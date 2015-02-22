b5a186ced8d85808a4e9661c5e043fe57fc50963
Philip Damra <pdamra@philipdamra.com>
2015-02-22 00:05:04 -0500

    full code

--- app.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('VocabularyTeacher', []);
-app.controller('LearnController', function($scope){
-    $scope.card = {
-        source: 'man',
-        target: 'hombre',
-        language: 'spanish'
-    }
-});
